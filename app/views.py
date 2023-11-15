8f8c4ac9c711170d8d7fea55709e36037947a523
jalford1 <jalford1@trinity.edu>
2023-11-15 10:31:34 -0600

    basic UI

--- app/views.py
+++ app/views.py
@@ -1,7 +1,20 @@
 from app import app
-from flask import flash
+from flask import flash, Flask, render_template
 
 
 @app.route("/")
-def helloworld():
-    return "Hello World"
+def home():
+    games = [
+        {"home_team": "Trinity", "away_team": "Southwestern", "sport": "Football", "date": "Nov 13", "time": "7PM"},
+        {"home_team": "Trinity", "away_team": "UT Dallas", "sport": "Volleyball", "date": "Nov 14", "time": "6PM"},
+        {"home_team": "Trinity", "away_team": "Hardin-Simmons", "sport": "Track and Field", "date": "Nov 18", "time": "11AM"},
+    ]
+    return render_template("home.html", games=games)
+
+@app.route("/team")
+def team():
+    return render_template("team.html")
+
+@app.route("/leaderboard")
+def leaderboard():
+    return render_template("leaderboard.html")
