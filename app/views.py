9ead613c1142ac19f71d4c843709cb60e633aa06
nathan early <Nathan Early>
2023-11-16 14:15:18 -0600

    change to radio buttons and add username submission

--- app/views.py
+++ app/views.py
@@ -5,9 +5,9 @@ from flask import flash, Flask, render_template
 @app.route("/")
 def home():
     games = [
-        {"home_team": "Trinity", "away_team": "Southwestern", "sport": "Football", "date": "Nov 13", "time": "7PM"},
-        {"home_team": "Trinity", "away_team": "UT Dallas", "sport": "Volleyball", "date": "Nov 14", "time": "6PM"},
-        {"home_team": "Trinity", "away_team": "Hardin-Simmons", "sport": "Track and Field", "date": "Nov 18", "time": "11AM"},
+        {"id": 1, "home_team": "Trinity", "away_team": "Southwestern", "sport": "Football", "date": "Nov 13", "time": "7PM"},
+        {"id": 2, "home_team": "Trinity", "away_team": "UT Dallas", "sport": "Volleyball", "date": "Nov 14", "time": "6PM"},
+        {"id": 3, "home_team": "Trinity", "away_team": "Hardin-Simmons", "sport": "Track and Field", "date": "Nov 18", "time": "11AM"},
     ]
     return render_template("home.html", games=games)
 
