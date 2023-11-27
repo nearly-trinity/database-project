20cdb1d357aee4b1893e0bd8c835ff8cc7b0c568
nathan early <Nathan Early>
2023-11-27 11:03:14 -0600

    database!

--- app/views.py
+++ app/views.py
@@ -5,3 +5,13 @@ from flask import flash
 @app.route("/")
 def helloworld():
     return "Hello World"
+
+
+@app.route("/login")
+def loginPage():
+    return "login here"
+
+
+@app.route("/test/<name>")
+def testArgument(name=None):
+    return "testing if the name works: " + str(name)
