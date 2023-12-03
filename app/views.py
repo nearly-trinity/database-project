02db0efb7f9f8724be510b162b0c72f9b15e69a2
jalford1 <jalford1@trinity.edu>
2023-12-03 16:04:38 -0600

    fix total predictions

--- app/views.py
+++ app/views.py
@@ -143,9 +143,9 @@ def leaderboard():
     users = cur.execute('''
         SELECT
             user.username,
-            COUNT(*) AS totalPredictions,
+            COUNT(CASE WHEN GR.event_id = UV.event_id THEN 1 ELSE 0 END) AS totalPredictions,
             SUM(CASE WHEN GR.winner_team_id = UV.chosen_winner_id THEN 1 ELSE 0 END) AS correctPredictions,
-            SUM(CASE WHEN GR.winner_team_id = UV.chosen_winner_id THEN 1 ELSE 0 END * 100) / COUNT(*) AS correctRatio
+            SUM(CASE WHEN GR.winner_team_id = UV.chosen_winner_id THEN 1 ELSE 0 END * 100) / COUNT(CASE WHEN GR.event_id = UV.event_id THEN 1 ELSE 0 END) AS correctRatio
         FROM UserVotes UV
         JOIN Users user ON user.user_id = UV.user_id
         LEFT JOIN GameResults GR ON GR.event_id = UV.event_id
